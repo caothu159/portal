5137bdf781794fc98ea9005f4bc8a241b726c715
Tran Ngoc Duc <caothu91@gmail.com>
2024-06-16 09:41:16 +0700

    - more env - disable welcome page

--- routes/web.php
+++ routes/web.php
@@ -8,7 +8,7 @@ declare(strict_types=1);
  * @author     Tran Ngoc Duc <ductn@diepxuan.com>
  * @author     Tran Ngoc Duc <caothu91@gmail.com>
  *
- * @lastupdate 2024-05-12 16:43:12
+ * @lastupdate 2024-06-16 09:38:53
  */
 
 use Illuminate\Support\Facades\Route;
@@ -24,4 +24,4 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', static fn () => view('welcome'));
+// Route::get('/', static fn () => view('welcome'));
