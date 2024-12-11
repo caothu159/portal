f9aa3e5062343eb09b87b31b921bb3aa8c8a6551
Tran Ngoc Duc <caothu91@gmail.com>
2024-12-11 18:25:26 +0700

    cai dat laravel/jetstream

--- routes/web.php
+++ routes/web.php
@@ -25,3 +25,13 @@ use Illuminate\Support\Facades\Route;
 */
 
 // Route::get('/', static fn () => view('welcome'));
+
+Route::middleware([
+    'auth:sanctum',
+    config('jetstream.auth_session'),
+    'verified',
+])->group(function () {
+    Route::get('/dashboard', function () {
+        return view('dashboard');
+    })->name('dashboard');
+});
--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,14 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
+import { globSync } from 'glob';
+
+const $assets = globSync('{diepxuan,node_modules}/**/resources/{js,css}/app.{js,css}')
+    .concat(globSync('resources/{js,css}/app.{js,css}'));
 
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: $assets,
             refresh: true,
         }),
     ],
